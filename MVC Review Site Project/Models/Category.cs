e46cab340e9666db7cdbd8854dcbfd2c39f7ce57
Austen Perelman-Hall <austenph@gmail.com>
2017-02-11 15:21:05 -0500

    created properties and keys for categories and reviews.

---
+++ MVC Review Site Project/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site_Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID {get; set;}
+        public string TechType { get; set; }
+
+        public virtual ICollection<Reviews> Reviews { get; set; }
+
+    }
+}
---
+++ MVC Review Site Project/Models/Reviews.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site_Project.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewContent { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Rating { get; set; }
+        public string Summary { get; set; }
+        public string ProductName { get; set; }
+        public string CompanyName { get; set; }
+        public int Price { get; set; }
+
+        [ForeignKey("Category")]//primary key in Category table
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
